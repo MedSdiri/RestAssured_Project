b83bdb6b0f956e3d13092c62cc397ed3b6c2339a
MedSdiri <sdiri.ad@gmail.com>
2021-03-25 09:47:24 -0500

    Added @DisplayName annotation to make the test resulsts more readable

--- src/test/java/day2/Junit5_Intro.java
+++ src/test/java/day2/Junit5_Intro.java
@@ -1,13 +1,30 @@
 package day2;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+//setting display name of the test class in test result using @Disp[layName
+
+
+@DisplayName("Testing numbers")
 public class Junit5_Intro {
 
+    @DisplayName("Testing numbers")
     @Test
-    public void test(){
+    public void test() {
+
+        System.out.println("Learning JUnit5");
+        //assertEquals(1,1);
+        //assertEquals(1, 2, "Trying out the error message");
 
     }
+    // add one more Test to assert your name first character starts with letter A
 
+    @DisplayName("Testing name")
+    @Test
+    public void testName(){
+        String name = "Ahmed";
+     assertEquals('A', name.charAt(0), "You gone crazy man");
+    }
 
 }
