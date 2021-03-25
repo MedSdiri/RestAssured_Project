aac0d8f472e5afe15d769a436ce3bfb56b3b3036
MedSdiri <sdiri.ad@gmail.com>
2021-03-25 10:23:16 -0500

    extras added

--- src/test/java/day2/Junit5_Intro.java
+++ src/test/java/day2/Junit5_Intro.java
@@ -9,12 +9,30 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Testing numbers")
 public class Junit5_Intro {
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("Before all is running !!!");
+    }
+
+    @AfterAll
+    public static void close(){
+        System.out.println("After all is running !!!!");
+    }
+    @BeforeEach
+    public void initEach(){
+        System.out.println("!!!!!! Before each is running !!!!!!!!");
+    }
+
+    @AfterEach
+    public void closeEach(){
+        System.out.println("????  After Each is running ????");
+    }
+
     @DisplayName("Testing numbers")
     @Test
     public void test() {
-
         System.out.println("Learning JUnit5");
-        //assertEquals(1,1);
+        assertEquals(1,1);
         //assertEquals(1, 2, "Trying out the error message");
 
     }
@@ -25,6 +43,9 @@ public class Junit5_Intro {
     public void testName(){
         String name = "Ahmed";
      assertEquals('A', name.charAt(0), "You gone crazy man");
+        System.out.println("Trying a very simple test");
     }
+
+
 
 }
