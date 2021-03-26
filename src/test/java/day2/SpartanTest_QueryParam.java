389f1f89b59f0a2e7bde80243b682f90bbf83987
MedSdiri <sdiri.ad@gmail.com>
2021-03-26 11:08:34 -0500

    Added chain requests

--- src/test/java/day2/SpartanTest_QueryParam.java
+++ src/test/java/day2/SpartanTest_QueryParam.java
@@ -1,4 +1,33 @@
 package day2;
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
 
-public class SpartanTest_QueryParam {
+import static org.hamcrest.MatcherAssert.*;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test_util.SpartanNoAuthBaseTest;
+
+public class SpartanTest_QueryParam extends SpartanNoAuthBaseTest{
+
+    @DisplayName("Test GET / spartans/ search Endpoint")
+    @Test
+    public void testSearch(){
+        Response response =
+                given()
+                        .log()
+                        .all()
+                .queryParam("nameContains", "Terence")
+                .queryParam("gender", "Male")
+                .when()
+                .get("/spartans/search")
+                .prettyPeek()
+                ;
+    }
+
 }
---
+++ src/test/java/day2/VerifyingResponseInTheChain.java
@@ -0,0 +1,41 @@
+package day2;
+
+
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+public class VerifyingResponseInTheChain extends SpartanNoAuthBaseTest {
+
+    @DisplayName("Verifying the GET /spartans/{id} response directly in the chain")
+    @Test
+    public void testOneSpartanInOneShot(){
+
+
+        given()
+                .log().all() // this will log the request
+                .pathParam("id",16).
+                when()
+                .get("/spartans/{id}").
+                then()
+                .log().all() // this will log the response
+                .statusCode(200)
+                .header("Content-Type", is("application/json") )
+                .contentType("application/json")
+                .body("id", equalTo(16) )
+                .body("name" , is("Wonder Woman") )
+                .body("gender" , is("Female") )
+                .body("phone" , equalTo(9234567890L))
+
+        ;
+
+
+    }
+
+}
