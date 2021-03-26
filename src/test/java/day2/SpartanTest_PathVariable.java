ec9fbff3bfb52f4c5a155e0e3cdd738975d8a9dc
MedSdiri <sdiri.ad@gmail.com>
2021-03-26 10:34:33 -0500

    Added test-util package and base URI and set the path

--- src/test/java/day2/SpartanTest_PathVariable.java
+++ src/test/java/day2/SpartanTest_PathVariable.java
@@ -5,53 +5,61 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import test_util.SpartanNoAuthBaseTest;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import test_util.SpartanNoAuthBaseTest;
 
 @DisplayName("Spartan Test with a Path variable")
 public class SpartanTest_PathVariable extends SpartanNoAuthBaseTest {
 
     @Test
-    public void getOneSpartan(){
-    //get("/spartans/100").prettyPrint();
+    public void getOneSpartan() {
+        //get("/spartans/100").prettyPrint();
 
-    // Iwant to provide 100 as path variable|parameter
-    // I want to provide accept header
-       Response r1 =
+        // Iwant to provide 100 as path variable|parameter
+        // I want to provide accept header
+        Response r1 =
                 given()
-                .header("Accept", "application/json")
-                .pathParam("spartan_id", "100")
-                .when()
-                .get("/spartans/{spartan_id}")
-               .prettyPeek()
-               ;
+                        .header("Accept", "application/json")
+                        .pathParam("spartan_id", "100")
+                        .when()
+                        .get("/spartans/{spartan_id}")
+                        .prettyPeek();
         ;
 
-       Response r2 =
-       given()
-                .accept("application/json")
-                .when()
-                .get("/spartans/{spartan_id}", 100)
-                .prettyPeek()
-                ;
+        Response r2 =
+                given()
+                        .accept("application/json")
+                        .when()
+                        .get("/spartans/{spartan_id}", 100)
+                        .prettyPeek();
 
-}
-@DisplayName("Logging the request")
-@Test
-    public void getOneSpartanWithLog(){}
+    }
 
-    Response response =
-            given()
-                    .log()
-                    .all()
-                    .accept("application/json")
-            .pathParam("id", "215")
-            .when()
-            .get("/spartans/{id}")
-            .prettyPeek()
-            ;
+    @DisplayName("Logging the request")
+    @Test
+    public void getOneSpartanWithLog() {
+        Response response =
+                given()
+                        .log()
+                        .all()
+                        .accept("application/json")
+                        .pathParam("id", "100")
+                        .when()
+                        .get("/spartans/{id}")
+                        .prettyPeek();
+
+
+        assertThat(response.statusCode() , equalTo(200) );
+        assertThat(response.contentType() , is("application/json"));
+        assertThat(response.path("name") , is("Wonder Woman")     );
+    }
+
+
 
 
 
