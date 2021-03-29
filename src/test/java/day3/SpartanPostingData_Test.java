0da6f6ff61b84b73562e6ae62465386d33a4da1a
MedSdiri <sdiri.ad@gmail.com>
2021-03-29 13:37:16 -0500

    POST Request with String completed

---
+++ src/test/java/day3/SpartanPostingData_Test.java
@@ -0,0 +1,79 @@
+package day3;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+@DisplayName("Testing adding data to Spartan app multiple way")
+public class SpartanPostingData_Test extends SpartanNoAuthBaseTest{
+
+    @DisplayName("POST /spartans with String")
+    @Test
+    public void testPostDataWithStringBody(){
+        /*
+        {
+        "name" : "Shannon"
+        "gander" : "Female"
+        "phone" : 1800233232
+        }
+         */
+
+        String postStrBody = "{\n" +
+                "                \"name\" : \"Shannon\",\n" +
+                "                \"gender\" : \"Female\",\n" +
+                "                \"phone\" : 1800233232\n" +
+                "            }" ;
+        given()
+                .log()
+                .all()
+        //      .contentType("application/json")
+                .contentType(ContentType.JSON)
+                .body(postStrBody)
+                .when()
+                .post("/spartans")
+                .then()
+                .log()
+                .all()
+                .statusCode(201)
+        .contentType(ContentType.JSON)
+        .body("success", is("A Spartan is Born!"))
+        .body("data.name", is("Shannon"))
+                ;
+
+
+
+
+    }
+
+    @DisplayName("POST /spartans with external file")
+    @Test
+    public void testPostDataWithJsonFileAsBody() {
+
+        File jsonFile = new File("singleSpartan.json");
+
+        given()
+                .log()
+                .all()
+                .contentType(ContentType.JSON)
+                .body(jsonFile)
+                .when()
+                .post("/spartans")
+                .then()
+                .log()
+                .all()
+                .statusCode(201)
+                ;
+
+
+    }
+
+}
