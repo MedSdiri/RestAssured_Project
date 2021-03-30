51ba4eb391273495c12e324b308806bc8c7222df
MedSdiri <sdiri.ad@gmail.com>
2021-03-30 10:37:40 -0500

    Practice to get data from POST and use it for GET to verify

--- src/test/java/day4/SpartanRandomPOST_Test.java
+++ src/test/java/day4/SpartanRandomPOST_Test.java
@@ -1,6 +1,7 @@
 package day4;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import pojo.Spartan;
 import spartan_util.SpartanUtil;
 import test_util.SpartanNoAuthBaseTest;
 import test_util.SpartanNoAuthBaseTest;
@@ -24,7 +25,7 @@ public class SpartanRandomPOST_Test extends SpartanNoAuthBaseTest {
 
     @DisplayName("/POST /spartans with random Data")
     @Test
-    public void addOneSpartanTest(){
+    public void addOneSpartanMAPTest(){
 
         Map<String, Object> payLoad = SpartanUtil.getRandomSpartanMap();
 
@@ -41,6 +42,71 @@ public class SpartanRandomPOST_Test extends SpartanNoAuthBaseTest {
         .body("data.gender", equalTo(payLoad.get("gender")))
         .body("data.phone", is(payLoad.get("phone")))
                 ;
+
+    }
+
+    @DisplayName("/POST /spartans with POJO")
+    @Test
+    public void addOneSpartanPOJOTest(){
+
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+
+        given()
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(randomPOJO)
+                .when()
+                .post("/spartans")
+                .then()
+                .log().all()
+                .statusCode(is(201))
+                .body("data.name", is(randomPOJO.getName()))
+                .body("data.gender", equalTo(randomPOJO.getGender()))
+                .body("data.phone", is(randomPOJO.getPhone()))
+        ;
+
+    }
+
+    @DisplayName("POST /spartans and send GET /spartans/{id} to verify")
+    @Test
+    public void testAddOneDataThenGetOneDataToVerify(){
+        // send post request, same the if that got generated
+        // check status code is 201
+        // send get request using the id saved from post request
+        // check status code is 200 and body match exactly what we sent
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+
+        Response response =
+                given()
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(randomPOJO)
+                .when()
+                .post("/spartans")
+                .prettyPeek()
+                ;
+        int newId = response.path("data.id");
+        System.out.println("newId = " + newId);
+        //int newId = response.jsonPath().getInt("data.id");
+        int statusCode = response.statusCode();
+        assertThat(response.statusCode(), is(201));
+
+        given()
+                .log()
+                .uri()
+                .pathParam("id", newId)
+                .when()
+                .get("/spartans/{id}")
+                .then()
+                .log()
+                .body()
+                .statusCode(200)
+        .body("id", is(newId))
+        .body("name", is(randomPOJO.getName()))
+        .body("gender", is(randomPOJO.getGender()))
+        .body("phone", is(randomPOJO.getPhone()))
+                ;
+
 
     }
 
