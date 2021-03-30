4d98a57b106009d1364a377d8cd623732f922cdc
MedSdiri <sdiri.ad@gmail.com>
2021-03-30 09:18:09 -0500

    SpartanUtil to create Random Spartan Map body starter

---
+++ src/test/java/spartan_util/SpartanUtil.java
@@ -0,0 +1,18 @@
+package spartan_util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SpartanUtil {
+
+
+    public static Map<String, Object> getRandomSpartanMap(){
+        Map<String, Object> bodyMap = new LinkedHashMap<>();
+        bodyMap.put("name", "Shannon");
+        bodyMap.put("gender", "Female");
+        bodyMap.put("phone", 1800233232L);
+
+        return bodyMap;
+    }
+
+}
