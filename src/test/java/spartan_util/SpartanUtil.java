acfaf291f8f68892c440a2a5f1c28f07bb2c90dc
MedSdiri <sdiri.ad@gmail.com>
2021-03-30 09:20:23 -0500

    Aded Java faker dependency

--- src/test/java/spartan_util/SpartanUtil.java
+++ src/test/java/spartan_util/SpartanUtil.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 public class SpartanUtil {
 
-
+    /**
+     * Used to get valid map object to represent post body fro POST/ spartans request
+     * @return Map object with random name, gender, phone number
+     */
     public static Map<String, Object> getRandomSpartanMap(){
         Map<String, Object> bodyMap = new LinkedHashMap<>();
         bodyMap.put("name", "Shannon");
