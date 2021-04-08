640512667bc08aaf71336dc349bbb810f233ee17
MedSdiri <sdiri.ad@gmail.com>
2021-04-08 11:32:14 -0500

    Added connection and removal into Spartan Base Test

--- src/test/java/day8/SpartanAPI_DB_Test.java
+++ src/test/java/day8/SpartanAPI_DB_Test.java
@@ -1,12 +1,16 @@
 package day8;
 
 import org.junit.jupiter.api.Test;
+import test_util.ConfigurationReader;
+import test_util.DB_Utility;
 import test_util.SpartanNoAuthBaseTest;
 
 public class SpartanAPI_DB_Test extends SpartanNoAuthBaseTest {
-
     @Test
     public void testDB_Connection(){
+        // this will create HR connection according to your properties file
+//        DB_Utility.createConnection();
+
 
     }
 }
