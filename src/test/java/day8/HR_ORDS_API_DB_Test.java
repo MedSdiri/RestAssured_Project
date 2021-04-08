b2e5de2e33784a388b1d0e16b7b8187a4213d2d4
MedSdiri <sdiri.ad@gmail.com>
2021-04-08 15:07:59 -0500

    Save All Regions Data from database into List of map

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,11 +1,14 @@
 package day8;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pojo.Region;
 import test_util.DB_Utility;
 import test_util.HR_ORDS_API_BaseTest;
 import test_util.HR_ORDS_API_BaseTest;
 import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static test_util.DB_Utility.*;
 import static io.restassured.RestAssured.*;
@@ -37,12 +40,15 @@ public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
 
         //get("/regions/{region_id}", 1)
 
+    }
 
-
-
-
-
-
+    @DisplayName("Test Get /regions and compare it with expected DB result")
+    @Test
+    public void testAllRegionsWithDB(){
+        runQuery("SELECT REGION_ID, REGION_NAME FROM REGIONS");
+        //saving all rows into List of Map
+        List<Map<String, String>> expectedRowMapList = getAllRowAsListOfMap();
+        System.out.println("expectedRowMapList = " + expectedRowMapList);
     }
 
 
