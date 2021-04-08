b67d548a247313a3f9e2deb9509bdb060352a167
MedSdiri <sdiri.ad@gmail.com>
2021-04-08 14:20:20 -0500

    HR DB ORDS initial test

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,5 +1,7 @@
 package day8;
 
+import org.junit.jupiter.api.Test;
+import test_util.DB_Utility;
 import test_util.HR_ORDS_API_BaseTest;
 import test_util.HR_ORDS_API_BaseTest;
 import java.util.List;
@@ -7,6 +9,13 @@ import static io.restassured.RestAssured.*;
 
 
 public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
+
+
+    @Test
+    public void hr_ORDS_TestWithDB(){
+        DB_Utility.runQuery("SELECT * FROM REGIONS");
+        DB_Utility.displayAllData();
+    }
 
 
 }
