ae698b85f4e0905b1494dbdc69eaf5163178d0fd
MedSdiri <sdiri.ad@gmail.com>
2021-04-08 14:37:22 -0500

    FIXED BASE URI ISSUE

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,11 +1,16 @@
 package day8;
 
 import org.junit.jupiter.api.Test;
+import pojo.Region;
 import test_util.DB_Utility;
 import test_util.HR_ORDS_API_BaseTest;
 import test_util.HR_ORDS_API_BaseTest;
 import java.util.List;
 import static io.restassured.RestAssured.*;
+import static test_util.DB_Utility.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
@@ -13,8 +18,25 @@ public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
 
     @Test
     public void hr_ORDS_TestWithDB(){
-        DB_Utility.runQuery("SELECT * FROM REGIONS");
-        DB_Utility.displayAllData();
+        runQuery("SELECT * FROM REGIONS WHERE REGION_ID = 1");
+        displayAllData();
+
+        Region r1 = given()
+                .pathParam("region_id", 1)
+                .get("/regions/{region_id}")
+                .jsonPath()
+                .getObject("items[0]", Region.class)
+                ;
+        System.out.println("r1 = " + r1);
+
+        //get("/regions/{region_id}", 1)
+
+
+
+
+
+
+
     }
 
 
