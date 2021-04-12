fc1be520b053f0d8d0c58b4d6668a2fce0e366ad
MedSdiri <sdiri.ad@gmail.com>
2021-04-12 11:19:12 -0500

    Formula One xml test get one driver info complete

--- src/test/java/day10/FormulaOneAPI_Test.java
+++ src/test/java/day10/FormulaOneAPI_Test.java
@@ -1,8 +1,11 @@
 package day10;
+import io.restassured.path.xml.XmlPath;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,8 +24,21 @@ public class FormulaOneAPI_Test {
     @Test
     public void testDriverOne(){
 
+        XmlPath xp =
+                given()
+                .pathParam("driver_id", "raikkonen")
+                .when()
+                .get("/drivers/{driver_id}")
+                .xmlPath()
+                ;
+        String givenName = xp.getString("MRData.DriverTable.Driver.GivenName");
+        String familyName = xp.getString("MRData.DriverTable.Driver.FamilyName");
+        System.out.println("givenName = " + givenName);
+        System.out.println("familyName = " + familyName);
+
 
     }
+
 
 
 
