8e1851893f40f39818c5216ef47062ff3455fdd2
MedSdiri <sdiri.ad@gmail.com>
2021-04-13 10:38:05 -0500

    extracting request specification and response specification for

---
+++ src/test/java/day11/SpecificationReuseTest.java
@@ -0,0 +1,54 @@
+package day11;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import test_util.SpartanWithAuthBaseTest;
+
+public class SpecificationReuseTest extends SpartanWithAuthBaseTest {
+    private static RequestSpecification requestSpec =
+            given()
+                    .auth().basic("admin", "admin")
+                    .accept(ContentType.JSON)
+            ;
+    private static ResponseSpecification responseSpec = expect()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            ;
+    @DisplayName("Admin GET /spartans and expect 200 and json")
+    @Test
+    public void testAdminGetAll(){
+
+
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/spartans")
+                .then()
+                .spec(responseSpec)
+//                .statusCode(200)
+//                .contentType(ContentType.JSON)
+                ;
+    }
+
+    @DisplayName("Admin GET /spartans/{id} and expect 200 and json, expect id match")
+    @Test
+    public void testAdminGetOne(){
+        given()
+                .auth().basic("admin", "admin")
+                .accept(ContentType.JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/spartans/{id}")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
